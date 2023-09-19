d848548f618aab81415a6d37a9e7954fe0bbedb5
xingjianll <4396kevinliu@gmail.com>
2023-09-19 10:17:48 -0400

    fixed code.

--- app/src/main/java/com/example/mobilegpt/viewmodel/global.kt
+++ app/src/main/java/com/example/mobilegpt/viewmodel/global.kt
@@ -5,7 +5,7 @@ import com.example.mobilegpt.socket.ClientSocket
 object global {
     var peerPort: Int = 4396
 //    10.0.2.2
-    var peerAddr: String = "13.55.198.181"
+    var peerAddr: String = ""
     var selfPort: Int? = null
     const val LOGIN: String = "00"
     const val CHAT: String = "01"
